84e5f9e0f0403f894fa36f0fb9ee0c297f2a419b
Łukasz G <lukasz.gretka@gmail.com>
2022-05-15 23:03:10 +0200

    Add measurement template, use ngmodel to measurement model

--- DP.Web/src/models/product.ts
+++ DP.Web/src/models/product.ts
@@ -1,11 +1,11 @@
 export class Product {
-    id: string;
-    name: string;
-    description: string;
-    //imagePath: string;
-    carbohydrates: number;
-    proteins: number;
-    fats: number;
-    calories: number;
-    barCode: number;
+  id: string;
+  name: string;
+  description: string;
+  //imagePath: string;
+  carbohydrates: number;
+  proteins: number;
+  fats: number;
+  calories: number;
+  barCode: number;
 }
