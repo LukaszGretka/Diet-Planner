19b5d99819a6534e7310a8b6b4d3f05fed903056
Łukasz G <lukasz.gretka@gmail.com>
2024-05-03 19:14:37 +0200

    Add missing migrations to user avatar

---
+++ DP.Backend/DietPlanner.Api/DTO/UserProfile/UserAvatarDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace DietPlanner.Api.DTO.UserProfile
+{
+    public class UserAvatarDTO
+    {
+        public string Base64Image { get; set; }
+    }
+}
---
+++ DP.Backend/DietPlanner.Api/Migrations/20240503171329_AddUserAvatarColumn.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DietPlanner.Api.Migrations
+{
+    public partial class AddUserAvatarColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
