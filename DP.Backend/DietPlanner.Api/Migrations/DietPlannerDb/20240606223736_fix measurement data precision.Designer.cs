a6d71931b79e7510500be9ed5dc921fb96f5062e
Łukasz G <lukasz.gretka@gmail.com>
2024-06-07 00:55:47 +0200

    Fix measurements

--- DP.Backend/DietPlanner.Api/Migrations/DietPlannerDb/20240606223736_fix measurement data precision.Designer.cs
+++ DP.Backend/DietPlanner.Api/Migrations/DietPlannerDb/20240606223736_fix measurement data precision.Designer.cs
@@ -4,6 +4,7 @@ using DietPlanner.Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DietPlanner.Api.Migrations.DietPlannerDb
 {
     [DbContext(typeof(DietPlannerDbContext))]
-    partial class DietPlannerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240606223736_fix measurement data precision")]
+    partial class fixmeasurementdataprecision
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
